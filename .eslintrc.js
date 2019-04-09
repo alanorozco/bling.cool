84b9ac4de2adf080001e5ab0f79805bbc6464c84
Alan Orozco <alan@orozco.xyz>
2019-04-08 19:49:30 -0700

    auto license

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   env: {
     es6: true,
   },
+  plugins: ['notice'],
   rules: {
     'no-unused-vars': [
       'error',
@@ -13,6 +14,13 @@ module.exports = {
         argsIgnorePattern: '^(_$|unused)',
         args: 'after-used',
         ignoreRestSiblings: false,
+      },
+    ],
+    'notice/notice': [
+      'error',
+      {
+        mustMatch: 'Copyright [0-9]{0,4} Alan Orozco',
+        templateFile: 'artifacts/license-header.txt',
       },
     ],
   },
