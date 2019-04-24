7bd7ca62f0827245e322248d0d3efbde49ee51df
Alan Orozco <alan@orozco.xyz>
2019-04-24 00:22:51 -0700

    Inline dirs in build

---
+++ builder/babel-plugins/bling-inline-dirs.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2019 Alan Orozco <alan@orozco.xyz>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const { dirs } = require('../../config');
+
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      MemberExpression(path) {
+        const { node } = path;
+        if (!node.object) {
+          return;
+        }
+        if (!t.isMemberExpression(node.object)) {
+          return;
+        }
+        if (!t.isIdentifier(node.object.object)) {
+          return;
+        }
+        if (!t.isIdentifier(node.property)) {
+          return;
+        }
+        if (!t.isIdentifier(node.object.property)) {
+          return;
+        }
+        if (node.object.object.name != 'dirs') {
+          return;
+        }
+        const dirType = node.object.property.name;
+        const dirProp = node.property.name;
+        if (!dirType in dirs) {
+          return;
+        }
+        if (!dirProp in dirs[dirType]) {
+          return;
+        }
+        path.replaceWith(t.stringLiteral(dirs[dirType][dirProp]));
+      },
+    },
+  };
+};
